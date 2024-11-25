2e1b94e827c51a892350448fbe476470bd8b03db
aramovsepyan2006@outlook.com <aramovsepyan2006@outlook.com>
2024-11-26 01:14:20 +0500

    Task3

--- Tyuiu.OvsepyanAA.Sprint4.Task3.V26.Lib/DataService.cs
+++ Tyuiu.OvsepyanAA.Sprint4.Task3.V26.Lib/DataService.cs
@@ -2,8 +2,20 @@
 
 namespace Tyuiu.OvsepyanAA.Sprint4.Task3.V26.Lib
 {
-    public class DataService
+    public class DataService : ISprint4Task3V26
     {
+        public int Calculate(int[,] array)
+        {
+            int maxInSecondRow = array[1, 0];
+            for (int j = 1; j < 5; j++)
+            {
+                if (array[1, j] > maxInSecondRow)
+                {
+                    maxInSecondRow = array[1, j];
+                }
+            }
 
+            return maxInSecondRow;
+        }
     }
 }
--- Tyuiu.OvsepyanAA.Sprint4.Task3.V26/Program.cs
+++ Tyuiu.OvsepyanAA.Sprint4.Task3.V26/Program.cs
@@ -6,7 +6,39 @@ namespace Tyuiu.OvsepyanAA.Sprint4.Task3.V26
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Спринт #4 | Выполнил: Овсепян А.А | СМАРТб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: ОВсепян А.А | СМАРТб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 2 до 6. Найдите максимальный     *");
+            Console.WriteLine("* элемент во второй строке массива.                                       *");
+            Console.WriteLine("* Массив:                                                                 *");
+            Console.WriteLine("* {4, 3, 2, 6, 2},                                                        *");
+            Console.WriteLine("* {5, 2, 2, 5, 6},                                                        *");
+            Console.WriteLine("* {2, 5, 4, 5, 6},                                                        *");
+            Console.WriteLine("* {4, 5, 2, 4, 6},                                                        *");
+            Console.WriteLine("* {6, 5, 4, 3, 6}                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService dataService = new DataService();
+
+            int[,] matrix = new int[5, 5]
+            {
+                { 4, 3, 2, 6, 2 },
+                { 5, 2, 2, 5, 6 },
+                { 2, 5, 4, 5, 6 },
+                { 4, 5, 2, 4, 6 },
+                { 6, 5, 4, 3, 6 }
+            };
+
+            Console.WriteLine($"Максимальный элемент: {dataService.Calculate(matrix)}");
+            Console.ReadKey();
         }
     }
 }
