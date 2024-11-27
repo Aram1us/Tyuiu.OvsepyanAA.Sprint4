5156ce332c0caabda6d0dd144c7f75ef1702e19e
aramovsepyan2006@outlook.com <aramovsepyan2006@outlook.com>
2024-11-27 08:00:15 +0500

    Task5

--- Tyuiu.OvsepyanAA.Sprint4.Task5.V2.Test/DataServiceTest.cs
+++ Tyuiu.OvsepyanAA.Sprint4.Task5.V2.Test/DataServiceTest.cs
@@ -8,6 +8,18 @@ namespace Tyuiu.OvsepyanAA.Sprint4.Task5.V2.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5] { { 0, -2, -4, 5, -4 },
+                                            { 4, -4, 5, -6, 7 },
+                                            { -7, -5, 6, 6, -6 },
+                                            { -6, -7, 6, 6, -4 },
+                                            { -5, -6, -6, -6, -5 } };
+
+            int wait = 7;
+
+            var rez = ds.Calculate(matrix);
+
+            Assert.AreEqual(wait, rez);
         }
     }
 }
